54faf712942400d04a4cc929a6ec615ae52a45cd
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-05-12 11:36:24 +0200

    Add IntStream for interfate

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,11 +1,16 @@
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
     public static void main(String[] args) {
         int number;
         for(int i = 0; i<100;i++){
             number = i+1;
             checkNumber(number);
-
         }
+        System.out.println("-----------------------------");
+        IntStream.iterate(1,i -> i+1)
+                .limit(100)
+                .forEach(FizzBuzz::checkNumber);
     }
 
     public static void checkNumber(int number){
