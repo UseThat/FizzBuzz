5fc1d88f29969e4c98b74481a21168a33e67cf96
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-05-12 11:32:31 +0200

    Add FizzBuzz logic

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,5 +1,28 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        
+        int number;
+        for(int i = 0; i<100;i++){
+            number = i+1;
+            checkNumber(number);
+
+        }
+    }
+
+    public static void checkNumber(int number){
+
+        if(number % 3 == 0 && number % 5 == 0){
+
+            System.out.println("FizzBuzz");
+        }else{
+            if(number % 3 == 0){
+
+                System.out.println("Fizz");
+            }else{
+                if(number % 5 == 0){
+
+                    System.out.println("Buzz");
+                }
+            }
+        }
     }
 }
